9b5fbe5ff7c819372a486ffd89517e5035877443
LoveNRage <55341386+LoveNRage@users.noreply.github.com>
2019-11-12 14:25:30 -0800

    Update CommandProcessor.java

--- src/framework/CommandProcessor.java
+++ src/framework/CommandProcessor.java
@@ -5,9 +5,9 @@ package framework;
  * 10/31/2019: NP - Created
  */
 public class CommandProcessor {
-	public static CommandProcessor commandProcessor = new CommandProcessor();
+	//public static CommandProcessor commandProcessor = new CommandProcessor();
 	
-	public void execute(Command c) {
+	public static void execute(Command c) {
 		c.execute();
 	}
 
